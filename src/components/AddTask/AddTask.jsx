559e6d94ade646241a35ba64405f8daa95b25c60
Krishna Kumar Gupta <kumarkrishn663@gmail.com>
2022-05-23 17:44:35 +0530

    data

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,13 +1,62 @@
-import React from "react";
+import React,{useState}from "react";
 import styles from "./addTask.module.css";
+import Tasks from "../Tasks/Tasks";
+import {v4 as uuid} from 'uuid'
 
 const AddTask = () => {
+  
+
+
+  const [value,setValue] = useState("")
+  const [todos,setTodos] = useState([])
+
+//   const handleChange =(e)=>{
+//     setValue(e.target.value)
+//     console.log(e.target.value)
+//   }
+
+//  const handleClick=()=>{
+//    setTodos([...todos,{
+//      value,
+//      id:uuid() ,
+//      status:false
+//    }]);
+//    setValue("")
+//  }
+
+
+
+const handleChange = (e)=>{
+  
+  setValue(e.target.value)
+  // console.log(todos)
+
+}
+const handleClick = ()=>{
+  // setTodos(todos)
+  setTodos([...todos,{value,
+      id: uuid(),
+  isCheck:false}]);
+  setValue("")
+}
+
+ 
+
+//  console.log(todos)
   // NOTE: do not delete `data-cy` key value pair
-  return (
+  return (<div>
+
     <div className={styles.todoForm}>
-      <input data-cy="add-task-input" type="text" />
-      <button data-cy="add-task-button"></button>
+
+      <input data-cy="add-task-input" placeholder="Add task..." type="text" onChange={handleChange} />
+
+      <button data-cy="add-task-button" disabled={!value} onClick={handleClick}>+</button>
+
     </div>
+    <div>
+      <Tasks value={value} todos={todos} />
+    </div>
+  </div>
   );
 };
 
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./task.module.css";
-
-const Task = () => {
+import Counter from '../Counter/Counter'
+const Task = (props) => {
   // NOTE: do not delete `data-cy` key value pair
+  // console.log(props.todos)
   return (
     <li data-cy="task" className={styles.task}>
       <input type="checkbox" data-cy="task-checkbox" />
-      <div data-cy="task-text"></div>
+      <div data-cy="task-text">
+      
+        {props.todos.map((elem)=>(
+        {elem}
+      ))}
+      </div>
       {/* Counter here */}
-      <button data-cy="task-remove-button"></button>
+      <Counter/>
+      <button data-cy="task-remove-button">X</button>
     </li>
   );
 };
